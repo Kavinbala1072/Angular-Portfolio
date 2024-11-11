b037e06e9d4ab215283d14588de9ab8dd3586e61
Kavinraj Balamurugan <kavinbala82@gmail.com>
2024-11-11 16:47:57 +0530

    Update project.component.ts

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './project.component.css'
 })
 export class ProjectComponent {
-
+downloadMarket(): void {
+    const fileUrl = '../Market Billing.rar'; 
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'Market Billing.rar';
+    link.click();
+  }
 }
